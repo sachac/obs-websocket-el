39698b547f5ac6bfd8375c77e8cb0a6656d768c5
Sacha Chua <sacha@sachachua.com>
2021-02-06 00:35:45 -0500

    Add websocket link, add minor mode for modeline display of scene and stream status

--- obs-websocket.el
+++ obs-websocket.el
@@ -9,20 +9,69 @@
   "Functions to call when messages arrive.")
 (defvar obs-websocket-debug nil "Debug messages")
 (defvar obs-websocket-message-callbacks nil "Alist of (message-id . callback-func)")
+(defvar obs-websocket-streaming-p nil "Non-nil if streaming.")
+(defvar obs-websocket-recording-p nil "Non-nil if recording.")
+(defvar obs-websocket-status "" "Modeline string.")
+(defvar obs-websocket-scene "" "Current scene.")
+
+(defun obs-websocket-update-mode-line ()
+  "Update the text for the mode line."
+  (let ((info
+         (concat "OBS:"
+                 obs-websocket-scene
+                 (if (or obs-websocket-streaming-p obs-websocket-recording-p)
+                     (concat
+                      "*"
+                      (if obs-websocket-streaming-p "Str" "")
+                      (if obs-websocket-recording-p "Rec" "")
+                      "*")
+                   ""))))
+    (setq obs-websocket-status info)))
+
+(define-minor-mode obs-websocket-minor-mode
+  "Minor mode for OBS Websocket."
+  :init-value nil
+  :lighter " OBS"
+  :global t
+  (let ((info '(obs-websocket-minor-mode obs-websocket-status " ")))
+    (if obs-websocket-minor-mode
+        ;; put in modeline
+        (add-to-list 'mode-line-misc-info info)
+      ;; remove from modeline
+      (setq mode-line-format
+            (seq-remove (lambda (x)
+                          (and (listp x) (equal (car x) 'obs-websocket-minor-mode)))
+                        mode-line-format)))))
 
 (defun obs-websocket-report-status (payload)
   (if (equal (plist-get payload :status) "error")
       (error "OBS: %s" (plist-get payload :error))
     (let ((msg
            (pcase (plist-get payload :update-type)
-             ("SwitchScenes" (format "Switched scene to %s" (plist-get payload :scene-name)))
+             ("SwitchScenes"
+              (setq obs-websocket-scene (plist-get payload :scene-name))
+              (format "Switched scene to %s" (plist-get payload :scene-name)))
              ("StreamStarting" "Stream starting.")
-             ("StreamStarted" "Stream started successfully.")
-             ("StreamStopped" "Stream stopped.")
-             ("RecordingStarting" "Recording starting.")
-             ("RecordingStopped" "Recording stopped")
-             ("StreamStatus" nil)
-             (_ (plist-get payload :update-type)))))
+             ("StreamStarted"
+              (setq obs-websocket-streaming-p t)
+              (obs-websocket-update-mode-line)
+              "Started streaming.")
+             ("StreamStopped"
+              (setq obs-websocket-streaming-p nil)
+              (obs-websocket-update-mode-line)
+              "Stopped streaming.")
+             ("RecordingStarting"
+              (setq obs-websocket-recording-p t)
+              (obs-websocket-update-mode-line)
+              "Started recording.")
+             ("RecordingStopped"
+              (setq obs-websocket-recording-p nil)
+              (obs-websocket-update-mode-line)
+              "Stopped recording")
+             ("StreamStatus"
+              (setq obs-websocket-streaming-p t)
+              nil)
+             )))
       (when msg (message "OBS: %s" msg)))))
 
 (defun obs-websocket-authenticate-if-needed (payload)
@@ -75,11 +124,21 @@
 
 (defun obs-websocket-connect (&optional url password)
   "Connect to an OBS instance."
-  (interactive (list (read-string "URL: " obs-websocket-url) nil))
+  (interactive (list (or obs-websocket-url (read-string "URL: ")) nil))
   (let ((obs-websocket-password (or password obs-websocket-password)))
     (setq obs-websocket (websocket-open (or url obs-websocket-url)
                                         :on-message #'obs-websocket-on-message
                                         :on-close #'obs-websocket-on-close))
-    (obs-websocket-send "GetAuthRequired")))
+    (obs-websocket-send "GetAuthRequired")
+    (obs-websocket-send "GetStreamingStatus"
+                        :callback (lambda (frame payload)
+                                    (setq obs-websocket-streaming-p (eq (plist-get payload :streaming) t))
+                                    (setq obs-websocket-recording-p (eq (plist-get payload :recording) t))
+                                    (obs-websocket-update-mode-line)))
+    (obs-websocket-send "GetCurrentScene"
+                        :callback (lambda (frame payload)
+                                    (setq obs-websocket-scene (plist-get payload :name))
+                                    (obs-websocket-update-mode-line)))
+    (obs-websocket-minor-mode 1)))
 
 (provide 'obs-websocket)
