179534d18aab728c9a8349920370ee3f14603a47
Charlie McMackin <charlie.mac@gmail.com>
2025-05-12 12:18:59 -0500

    Ensure OBS message debug logging before JSON parsing
    JSON parsing could have errors. Let's log first thing.

--- obs-websocket.el
+++ obs-websocket.el
@@ -149,11 +149,10 @@
 
 (defun obs-websocket-on-message (websocket frame)
   "Handle OBS WEBSOCKET sending FRAME."
+  (when obs-websocket-debug
+    (setq obs-websocket-messages (cons frame obs-websocket-messages)))
   (let* ((payload (json-parse-string (websocket-frame-payload frame)
                                      :object-type 'plist :array-type 'list)))
-    (when obs-websocket-debug
-      (setq obs-websocket-messages (cons frame obs-websocket-messages)))
-
     (run-hook-with-args 'obs-websocket-on-message-payload-functions payload)))
 
 (defun obs-websocket-on-close (&rest args)
